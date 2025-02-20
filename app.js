bd890074add227f6086c820f1c175b634a506d4e
company <jms010802@naver.com>
2025-02-20 17:51:15 +0900

    First

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const connectDb = require("./config/db");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+connectDb();
+
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use("/", require("./routes/main"));
+app.use("/", require("./routes/admin"));
+
+app.listen(port, () => {
+    console.log(`App Listening on port ${port}`);
+})
