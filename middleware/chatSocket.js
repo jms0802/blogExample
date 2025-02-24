73b35b71b42d21c25bb2797bfc3570e088a29f8b
company <jms010802@naver.com>
2025-02-24 17:53:47 +0900

    채팅 소유자 판별

--- middleware/chatSocket.js
+++ middleware/chatSocket.js
@@ -17,17 +17,20 @@ module.exports = (server) => {
             connectUser.push(username);
             socket.name = username;
             io.emit("login", username + " 님이 입장하였습니다.");
+            socket.emit("login-success", username);
             console.log(`${username} 연결됨`);
         });
 
 
         //이전 채팅 기록 표시
-        socket.emit("chat history", messages, socket.name);
+        socket.emit("chat history", messages);
 
         //채팅 입력
-        socket.on("chat message", (msg) => {
+        socket.on("chat message", (data) => {
+            const msg = data.msg;
+            const user = data.user;
             messages.push(msg);
-            io.emit("chat message", msg);
+            io.emit("chat message", { msg: msg, user: user });
         });
 
         socket.on('forceDisconnect', function () {
