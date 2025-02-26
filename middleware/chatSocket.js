184ae1976676f308c06bc184b9dadeddd7b5c62a
company <jms010802@naver.com>
2025-02-26 17:55:23 +0900

    채팅 접속 인원 확인 팝업창

--- middleware/chatSocket.js
+++ middleware/chatSocket.js
@@ -8,7 +8,7 @@ let connectUser = [];
 module.exports = (server) => {
     const io = SocketIO(server, { path: "/socket.io" });
     io.on("connection", (socket) => {
-        if(connectUser.length >= MAX_CONNECT){
+        if (connectUser.length >= MAX_CONNECT) {
             socket.emit("redirect", "/");
             socket.disconnect();
             return;
@@ -16,33 +16,42 @@ module.exports = (server) => {
         //접속 유저명 표시
         socket.on("login", function (data) {
             let username = data.name;
+            let time = data.connectionTime;
 
             while (connectUser.includes(username)) {
                 username = "User" + Math.floor(Math.random() * 100);
             }
 
-            connectUser.push(username);
+            connectUser.push({ username, connectionTime: time });
             socket.name = username;
-            io.emit("login-msg", username + " 님이 입장하였습니다.");
-            socket.emit("login-success", username);
+
+            const loginMsg = username + " 님이 입장하였습니다.";
+            io.emit("login-msg",
+                {
+                    msg: loginMsg,
+                    count: connectUser.length,
+                    maxcount: MAX_CONNECT,
+                    user: connectUser[connectUser.length - 1]
+                });
+            socket.emit("login-success", { username: username });
             console.log(`${username} 연결됨`);
         });
 
 
         //이전 채팅 기록 표시
-        socket.emit("chat history", messages);
+        socket.emit("chat history", { messages: messages, users: connectUser });
 
         //채팅 입력
         socket.on("chat message", (data) => {
             const msg = data.msg;
             const user = data.user;
             const date = data.createDate;
-            if(messages.length >= MAX_MESSAGE){
+            if (messages.length >= MAX_MESSAGE) {
                 messages.shift();
             }
-            messages.push({msg, user, createDate:date});
+            messages.push({ msg, user, createDate: date });
             console.log(messages);
-            io.emit("chat message", { msg: msg, user: user, createDate:date });
+            io.emit("chat message", { msg: msg, user: user, createDate: date });
         });
 
         socket.on('forceDisconnect', function () {
@@ -50,9 +59,24 @@ module.exports = (server) => {
         });
         //사용자 연결 해제
         socket.on("disconnect", () => {
-            console.log(`${socket.name} 연결 해제`);
-            connectUser.pop(socket.name);
-            io.emit("disconnectUser", socket.name + " 님이 퇴장하였습니다.");
+            let removeUser = connectUser.find(u => u.username === socket.name);
+            let msg = "";
+            if (removeUser) {
+                let index = connectUser.indexOf(removeUser);
+                if (index !== -1) {
+                    connectUser.splice(index, 1);
+                }
+                console.log(`${removeUser.username} 연결 해제`);
+                msg = removeUser.username + " 님이 퇴장하였습니다.";
+            } else {
+                msg = "undefined";
+            }
+            io.emit("disconnectUser", {
+                msg: msg,
+                count: connectUser.length,
+                maxcount: MAX_CONNECT,
+                user: removeUser
+            });
         });
     });
 };
