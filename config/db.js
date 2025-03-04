ccde572415e1cbfe634fe52ce07a872653d12330
PKdorm <jms010802@naver.com>
2025-03-04 22:38:33 +0900

    DB 환경변수를 개발용/제품용 으로 나누기

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
 const asyncHandler = require("express-async-handler");
-require("dotenv").config();
+const fs = require('fs');
+const path = require('path');
+
+const envFile = process.env.NODE_ENV === 'product' ? '.env.product' : '.env.dev';
+require('dotenv').config({ path: path.resolve(__dirname, '..', envFile) });
 
 const connectDb = asyncHandler( async() => {
     try {
         const connect = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`DB Connected : ${connect.connection.host}`);
-    } catch(err) {
-        console.log(err);
+    } catch(error) {
+        console.error('MongoDB 연결 실패:', error);
+        process.exit(1);
     }
 });
 
