09a10d9f40fb4ef82ddde92f21bc3af956b527c4
PKdorm <jms010802@naver.com>
2025-03-04 10:37:37 +0900

    다크모드 적용

--- config/db.js
+++ config/db.js
@@ -3,8 +3,12 @@ const asyncHandler = require("express-async-handler");
 require("dotenv").config();
 
 const connectDb = asyncHandler( async() => {
-    const connect = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`DB Connected : ${connect.connection.host}`);
+    try {
+        const connect = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`DB Connected : ${connect.connection.host}`);
+    } catch(err) {
+        console.log(err);
+    }
 });
 
 module.exports = connectDb;
