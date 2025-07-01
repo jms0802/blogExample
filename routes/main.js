2d9d11acec23534f85f6ba37fa08ad1cb5ed336d
jms0802 <jms010802@naver.com>
2025-07-01 20:22:50 +0900

    모든 게시물 페이지 추가

--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,10 @@ const Post = require("../models/Post");
 const marked = require('marked');
 const asyncHandler = require("express-async-handler");
 
+/**
+ * 메인 페이지
+ * GET /
+ */
 router.get(["/", "/home"], asyncHandler(async (req, res) => {
     const locals = {
         title: "메인 페이지"
@@ -13,6 +17,10 @@ router.get(["/", "/home"], asyncHandler(async (req, res) => {
     res.render("index", { locals, data, layout: mainLayout });
 }));
 
+/**
+ * 실시간 채팅 페이지
+ * GET /chat
+ */
 router.get("/chat", (req, res) => {
     const locals = {
         title: "실시간 채팅",
@@ -36,5 +44,21 @@ router.get("/post/:id", asyncHandler(async (req, res) => {
     }
     res.render("post", { locals, data, content: content, layout: mainLayout });
 }));
+
+/**
+ * 모든 게시물 보기
+ * GET /blog
+ */
+router.get('/blog', async (req, res) => {
+    try {
+        const posts = await Post.find({}).sort({ createAt: -1 });
+        const locals = {
+            title: "모든 게시물",
+        }
+        res.render('blog', { locals, posts, layout: mainLayout });
+    } catch (err) {
+        res.status(500).send('게시물을 불러오는 중 오류가 발생했습니다.');
+    }
+});
 
 module.exports = router;
